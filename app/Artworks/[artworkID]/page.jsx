46575654324d69dc37c2697ff56d8e4f654c35ff
King Artur <109923283+M4A1SD@users.noreply.github.com>
2024-12-31 11:52:26 +0200

    changed mockdata

--- app/Artworks/[artworkID]/page.jsx
+++ app/Artworks/[artworkID]/page.jsx
@@ -1,10 +1,7 @@
 export default async function ArtworkPage({ params }) {
-    // params.artistId will be "23" when URL is "http://localhost:3000/Artists/23"
+    // params.artistId will be "23" when URL is "http://localhost:3000/Artworks/23"
     const { artworkID } = params;
-    console.log('Artist ID from URL:', artworkID); // This will log "23"
   
-    // Simulate fetching or using artistId
-    // Try to fetch the data from DB
     const artworkData = { id: artworkID, name: `Artist ${artworkID}` };
   
     return (
