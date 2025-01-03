1fafc375690f73adad35da8579cc942311998b8a
King Artur <109923283+M4A1SD@users.noreply.github.com>
2025-01-03 17:18:22 +0200

    restore back to use mock

--- app/Artworks/page.jsx
+++ app/Artworks/page.jsx
@@ -1,25 +1,11 @@
-// app/artworks/page.js
-
 "use client";
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { useRouter } from "next/navigation";
+import artworksData from "./mockArtworks.json";
 import Image from "next/image";
 
 const ArtworksPage = () => {
-  const [artworksData, setArtworksData] = useState([]);
   const router = useRouter();
-
-  // Fetch artworks data when the component mounts
-  useEffect(() => {
-    const fetchArtworks = async () => {
-      // Call the API route to get the artworks
-      const res = await fetch('/api/artwork');
-      const data = await res.json();
-      setArtworksData(data); // Set fetched data into the state
-    };
-
-    fetchArtworks();
-  }, []);
 
   const handleArtworkClick = (artworkId) => {
     router.push(`/Artworks/${artworkId}`);
@@ -32,35 +18,35 @@ const ArtworksPage = () => {
       </div>
 
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-        {artworksData.length === 0 ? (
-          <p>No artworks found.</p>
-        ) : (
-          artworksData.map((artwork) => (
-            <div
-              key={artwork._id}
-              className="bg-white dark:bg-gray-800 rounded-lg shadow-lg overflow-hidden 
-                hover:shadow-2xl hover:scale-[1.02] hover:ring-2 hover:ring-blue-400 dark:hover:ring-blue-500
-                transition-all duration-300 cursor-pointer"
-              onClick={() => handleArtworkClick(artwork._id)}
-            >
-              <div className="flex flex-col">
-                <div className="w-full h-48 overflow-hidden relative">
-                  <Image
-                    src={artwork.picture}
-                    alt={artwork.title}
-                    fill
-                    className="object-cover transition-transform duration-300 group-hover:scale-110"
-                  />
-                </div>
-                <div className="p-6">
-                  <h3 className="text-xl font-semibold mb-2">{artwork.title}</h3>
-                  <p className="text-gray-600 dark:text-gray-300 mb-2">🎨 {artwork.artistName}</p>
-                  <p className="text-gray-700 dark:text-gray-400 line-clamp-3">ℹ️ {artwork.description}</p>
-                </div>
+        {artworksData.artworks.map((artwork) => (
+          <div
+            key={artwork.id}
+            className="bg-white dark:bg-gray-800 rounded-lg shadow-lg overflow-hidden 
+            hover:shadow-2xl hover:scale-[1.02] hover:ring-2 hover:ring-blue-400 dark:hover:ring-blue-500
+            transition-all duration-300 cursor-pointer"
+            onClick={() => handleArtworkClick(artwork.id)}
+          >
+            <div className="flex flex-col">
+              <div className="w-full h-48 overflow-hidden relative">
+                <Image
+                  src={artwork.picture}
+                  alt={artwork.title}
+                  fill
+                  className="object-cover transition-transform duration-300 group-hover:scale-110"
+                />
+              </div>
+              <div className="p-6">
+                <h3 className="text-xl font-semibold mb-2">{artwork.title}</h3>
+                <p className="text-gray-600 dark:text-gray-300 mb-2">
+                  🎨 {artwork.artistName}
+                </p>
+                <p className="text-gray-700 dark:text-gray-400 line-clamp-3">
+                  ℹ️ {artwork.description}
+                </p>
               </div>
             </div>
-          ))
-        )}
+          </div>
+        ))}
       </div>
     </div>
   );
