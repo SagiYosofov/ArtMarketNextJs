26a341f2054646c62c05f8db73f1b510dcb74e48
King Artur <109923283+M4A1SD@users.noreply.github.com>
2025-01-14 18:43:43 +0200

    comments

--- app/Cart/page.jsx
+++ app/Cart/page.jsx
@@ -2,17 +2,16 @@
 import React, { useState, useEffect } from "react";
 import ItemComponent from "../Artworks/ItemComponent";
 import data from "../Artworks/dbArtworks.json";
-import { useRouter } from "next/navigation";
 
 const CartPage = () => { 
-  const router = useRouter();
   const [artworkData, setArtworkData] = useState([]);
 
   useEffect(() => {
-    // Get cart IDs from localStorage
-
+    // Get saved art from localStorage
     const cartData = JSON.parse(localStorage.getItem('artGalleryCart') || '[]');
 
+    // javascript AI hacks dont work, use for i loop.
+    // create an array of dictionary, to forward it to ItemComponent.
     const artworkData = [];
     for (let i = 0; i < cartData.length; i++) {
       const artId = cartData[i].id;
@@ -23,33 +22,31 @@ const CartPage = () => {
         }
       }
     }
+    // set state, to proc the useEffect.
     setArtworkData(artworkData);
   }, []);
 
   const handleRemoveItem = (itemId) => {
-    // Remove from state
+    // remove locally first to give visual feedback.
+    setArtworkData(artworkData.filter(item => item.id !== itemId));
+
     // Remove from localStorage
+    // get the current local storage cart array
     const cartIds = JSON.parse(localStorage.getItem('artGalleryCart') || '[]');
-    // const updatedCart = cartIds.filter(id => id !== itemId);
+    // find the item to remove
     for (let i = 0; i < cartIds.length; i++) {
       if (cartIds[i].id === itemId) {
         cartIds.splice(i, 1);
         break;
       }
     }
+    // update local storage with the new array
     localStorage.setItem('artGalleryCart', JSON.stringify(cartIds));
   };
 
 
-  const handleArtworkClick = (artworkId) => {
-    router.push(`/Artworks/${artworkId}`);
-  };
 
-  const deleteItem = (itemId) => {
-    handleRemoveItem(itemId);
-    setArtworkData(artworkData.filter(item => item.id !== itemId));
-  };
-
+  
   return (
     <div className="mt-20 container mx-auto px-4 relative z-0">
       <h2 className="text-3xl font-bold mb-8">🛒 Your Cart</h2>
@@ -60,7 +57,7 @@ const CartPage = () => {
       {artworkData.length > 0 ? (
         <ItemComponent 
           artworksData={artworkData}
-          handleArtworkClick={deleteItem}
+          handleArtworkClick={handleRemoveItem}
         />
       ) : (
         <p className="text-gray-600 dark:text-gray-300 text-center">
