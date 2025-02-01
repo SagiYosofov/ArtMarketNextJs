815f16702bf736c43b87e4dd33ae5270993e63cb
DanielFeldman1 <daniel.feldman303@gmail.com>
2025-02-01 12:34:43 +0200

    Separated technologies form techStack comp

--- components/AboutPageComponents/techStack.jsx
+++ components/AboutPageComponents/techStack.jsx
@@ -1,15 +1,8 @@
 import Image from 'next/image'
+import technologies from '@/constants/technologies'
 // Returns a UI element with technologies we used in out project
 const TechStack = () => {
-  // Technology names and svg paths
-  const technologies = [
-    { name: 'Next.js', logo: '/about/techStack/nextjs.svg' },
-    { name: 'MongoDB', logo: '/about/techStack/mongodb.svg' },
-    { name: 'Node.js', logo: '/about/techStack/nodejs.svg' },
-    { name: 'GitHub', logo: '/about/techStack/github.svg' },
-    { name: 'Vercel', logo: '/about/techStack/vercel.svg' },
-    { name: 'Tailwind CSS', logo: '/about/techStack/tailwind.svg' }
-  ]
+  
   // Return UI element
   return (
     <div className="mt-16 mb-16">
