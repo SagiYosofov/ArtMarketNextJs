7336c1cb61a4b470ab9040067b3a8f6f61d2c2bf
DanielFeldman1 <daniel.feldman303@gmail.com>
2025-02-01 12:29:04 +0200

    Separated constants from team line up comp

--- components/AboutPageComponents/TeamLineUp.jsx
+++ components/AboutPageComponents/TeamLineUp.jsx
@@ -1,35 +1,9 @@
 import React from "react"
 import Image from "next/image"
 import Link from "next/link"
+import teamMembers from "@/constants/teamMembersConstants"
 // Returns a div with the team's photos and details
 const TeamLineUp = () => {
-  // Team data
-    const teamMembers = [
-        {
-            name: "Arthur Cherniy",
-            role: "",
-            image: "/about/dev team/arthur.png",
-            linkedin: "https://www.linkedin.com/in/arthur-cherniy-267946241/",
-        },
-        {
-            name: "Sagie Yosofov",
-            role: "",
-            image: "/about/dev team/sagi.jpg",
-            linkedin: "https://www.linkedin.com/in/sagieyosofov/",
-        },
-        {
-            name: "Daniel Feldman",
-            role: "",
-            image: "/about/dev team/daniel.png",
-            linkedin: "https://www.linkedin.com/in/dany-feldman/",
-        },
-        {
-            name: "Aviv Raz",
-            role: "",
-            image: "/about/dev team/aviv.png",
-            linkedin: "https://www.linkedin.com/in/aviv-raz-59a541b8/",
-        },
-    ]
     // UI element
     return (
         <div className="my-20">
